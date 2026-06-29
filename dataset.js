6d283b0c7f9343bf33a36ec7de592c1c6dd97e20
thabisolence <lencethabiso@gmail.com>
2026-06-29 20:02:01 +0200

    Create dataset.js

---
+++ dataset.js
@@ -0,0 +1,7 @@
+const routes = [
+  { id:"A", name:"Route A", distance_km:10, estimated_time_min:18, traffic_level:"High", fuel_litres:1.2, co2_kg:2.8 },
+  { id:"B", name:"Route B", distance_km:14, estimated_time_min:20, traffic_level:"Low", fuel_litres:0.9, co2_kg:2.1 },
+  { id:"C", name:"Route C", distance_km:12, estimated_time_min:19, traffic_level:"Medium", fuel_litres:1.0, co2_kg:2.4 },
+  { id:"D", name:"Route D", distance_km:9, estimated_time_min:16, traffic_level:"Very High", fuel_litres:1.4, co2_kg:3.0 },
+  { id:"E", name:"Route E", distance_km:16, estimated_time_min:22, traffic_level:"Low", fuel_litres:0.85, co2_kg:1.9 }
+];
